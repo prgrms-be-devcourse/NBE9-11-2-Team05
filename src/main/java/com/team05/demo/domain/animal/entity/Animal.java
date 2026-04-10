af73f218e50d737cae1a538d1c1a2d8b56d283d8
alicek0 <alicekangisy@gmail.com>
2026-04-11 00:33:45 +0900

    chore: BaseEntity, Animal, Feed - 기본 엔티티 및 간단하게 animal, feed entity에 id 추가

--- src/main/java/com/team05/demo/domain/animal/entity/Animal.java
+++ src/main/java/com/team05/demo/domain/animal/entity/Animal.java
@@ -1,4 +1,16 @@
 package com.team05.demo.domain.animal.entity;
 
-public class Animal {
+import com.team05.demo.global.common.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Animal extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
--- src/main/java/com/team05/demo/domain/feed/entity/Feed.java
+++ src/main/java/com/team05/demo/domain/feed/entity/Feed.java
@@ -1,4 +1,13 @@
 package com.team05.demo.domain.feed.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Entity
+@Getter
 public class Feed {
+    @Id
+    private Long id;
+
 }
