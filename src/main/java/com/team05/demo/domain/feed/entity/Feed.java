29b5d941830de441237ce6edf39c6d963d04aa09
501501a <2501agency@gmail.com>
2026-04-11 22:25:22 +0900

    feat: feed entity 추가

--- src/main/java/com/team05/demo/domain/feed/entity/Feed.java
+++ src/main/java/com/team05/demo/domain/feed/entity/Feed.java
@@ -1,13 +1,35 @@
 package com.team05.demo.domain.feed.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import com.team05.demo.global.entity.BaseEntity;
+import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
-public class Feed {
+@NoArgsConstructor
+@Table(name = "feeds")
+public class Feed extends BaseEntity {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    private User user;    // FK
+    @Column(nullable = false)
+    private Long userId;
+
+    private String desertionNo; // FK
+
+    @Column(nullable = false)
+    private String category;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    private String imageUrl;
 
 }
