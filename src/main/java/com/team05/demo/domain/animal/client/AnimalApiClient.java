ba5b456fb163c5c567049effef544eba58938e4f
DonggunSe0 <cloudhenry1@gmail.com>
2026-04-11 13:16:26 +0900

    chore : AnimalApiProperties 및 AnimalApiClient 파일 추가

---
+++ src/main/java/com/team05/demo/domain/animal/client/AnimalApiClient.java
@@ -0,0 +1,4 @@
+package com.team05.demo.domain.animal.client;
+
+public class AnimalApiClient {
+}
---
+++ src/main/java/com/team05/demo/domain/animal/config/AnimalApiProperties.java
@@ -0,0 +1,4 @@
+package com.team05.demo.domain.animal.config;
+
+public class AnimalApiProperties {
+}
