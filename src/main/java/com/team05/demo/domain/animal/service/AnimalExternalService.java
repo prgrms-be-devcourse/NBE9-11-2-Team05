722c9c9dee33203d19880d3857cab2441efd94f3
DonggunSe0 <cloudhenry1@gmail.com>
2026-04-12 19:44:04 +0900

    feat: 외부 유기동물 목록 API 호출 서비스 추가

---
+++ src/main/java/com/team05/demo/domain/animal/service/AnimalExternalService.java
@@ -0,0 +1,29 @@
+package com.team05.demo.domain.animal.service;
+
+import com.team05.demo.domain.animal.client.AnimalApiClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+
+// 동물 관련 외부 API와의 통신을 담당하는 서비스 클래스
+@Service
+@RequiredArgsConstructor
+public class AnimalExternalService {
+    private final AnimalApiClient animalApiClient;
+
+    // 유기동물 목록을 API에서 조회하는 메서드
+    public String fetchAnimals() {
+        String url = animalApiClient.getAbandonmentUrl()
+                + "?serviceKey=" + animalApiClient.getServiceKey()
+                + "&pageNo=1"
+                + "&numOfRows=10"
+                + "&_type=" + animalApiClient.getReturnType();
+
+        return RestClient.create()
+                .get()
+                .uri(url)
+                .retrieve()
+                .body(String.class);
+    }
+
+}
