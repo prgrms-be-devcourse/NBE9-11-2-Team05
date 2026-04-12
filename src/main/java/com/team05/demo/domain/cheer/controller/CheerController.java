77f5098da9431f867bfee9f8b8285a3b3d589a4f
perandowind <perandowind@gmail.com>
2026-04-12 17:25:47 +0900

    cheerController 메서드 정의

--- src/main/java/com/team05/demo/domain/cheer/controller/CheerController.java
+++ src/main/java/com/team05/demo/domain/cheer/controller/CheerController.java
@@ -1,4 +1,39 @@
 package com.team05.demo.domain.cheer.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+@Tag(name = "CheerController", description = "응원 API")
 public class CheerController {
+
+
+    @GetMapping("/cheers/today")
+    @Operation(summary = "잔여 응원 횟수 조회")
+    public void getTodaysCheers() {
+
+    }
+
+
+    @GetMapping("/animals/{animalId}/cheers")
+    @Operation(summary = "응원 수 및 온도 조회")
+    public void getAnimalCheers() {
+
+    }
+
+
+    @PostMapping("/animals/{animalId}/cheers")
+    @Operation(summary = "응원 부여")
+    public void cheerAnimal() {
+
+    }
+
+
+
+
 }
---
+++ src/main/java/com/team05/demo/domain/cheer/service/CheerService.java
@@ -0,0 +1,16 @@
+package com.team05.demo.domain.cheer.service;
+
+import com.team05.demo.domain.cheer.repository.CheerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CheerService {
+
+    private final CheerRepository cheerRepository;
+
+
+
+
+}
