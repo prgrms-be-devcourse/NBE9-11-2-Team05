1e830242231b3b55ad7bb65429a7e2c463399242
DonggunSe0 <cloudhenry1@gmail.com>
2026-04-13 15:45:14 +0900

    fix: main 반영 후 충돌 해결

--- src/main/java/com/team05/demo/domain/animal/repository/AnimalRepository.java
+++ src/main/java/com/team05/demo/domain/animal/repository/AnimalRepository.java
@@ -1,4 +1,21 @@
 package com.team05.demo.domain.animal.repository;
 
-public class AnimalRepository {
+import com.team05.demo.domain.animal.entity.Animal;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface AnimalRepository extends JpaRepository<Animal, Long> {
+    // 유기번호로 조회
+    Optional<Animal> findByDesertionNo(String desertionNo);
+    boolean existsByDesertionNo(String desertionNo);
+
+    // 원자적 UPDATE
+    @Modifying
+    @Query("UPDATE Animal a SET a.totalCheerCount = a.totalCheerCount + 1 " +
+            "WHERE a.id = :animalId")
+    void incrementCheerCount(@Param("animalId") Long animalId);
 }
