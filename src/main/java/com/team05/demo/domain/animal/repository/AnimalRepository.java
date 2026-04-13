e5dcac99fbf8edb44e8591f132866fc81f10dd4e
DonggunSe0 <cloudhenry1@gmail.com>
2026-04-13 11:27:49 +0900

    feat: AnimalRepository Jpa 적용 및 추가

--- src/main/java/com/team05/demo/domain/animal/repository/AnimalRepository.java
+++ src/main/java/com/team05/demo/domain/animal/repository/AnimalRepository.java
@@ -1,4 +1,11 @@
 package com.team05.demo.domain.animal.repository;
 
-public class AnimalRepository {
+import com.team05.demo.domain.animal.entity.Animal;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AnimalRepository extends JpaRepository<Animal, Long> {
+    Optional<Animal> findByDesertionNo(String desertionNo);
+    boolean existsByDesertionNo(String desertionNo);
 }
