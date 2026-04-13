0aec229d0bac4579612c21dd50d6a1f434146b88
alicek0 <alicekangisy@gmail.com>
2026-04-13 17:45:31 +0900

    fix: CommentRes 명세서 맞게 수정 및 null방지

--- src/main/java/com/team05/demo/domain/comment/dto/CommentRes.java
+++ src/main/java/com/team05/demo/domain/comment/dto/CommentRes.java
@@ -1,20 +1,26 @@
 package com.team05.demo.domain.comment.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.team05.demo.domain.comment.entity.Comment;
 
+import java.time.LocalDateTime;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record CommentRes (
     Long commentId,
     String content,
-    String targetType, // FEED or ANIMAL
-    Long targetId
+    Long feedId,    // 유기동물 댓글이면 null이 들어감
+    Long animalId,
+    LocalDateTime createdAt
 ) {
     public static CommentRes from (Comment comment){
         boolean isFeedComment = comment.getFeed()!=null;
         return new CommentRes(
                 comment.getId(),
                 comment.getContent(),
-                isFeedComment ? "FEED" : "ANIMAL",
-                isFeedComment ? comment.getFeed().getId() : comment.getAnimal().getId()
+                comment.getFeed() != null ? comment.getFeed().getId() : null,
+                comment.getAnimal() != null ? comment.getAnimal().getId() : null,
+                comment.getCreatedAt()
         );
     }
 }
