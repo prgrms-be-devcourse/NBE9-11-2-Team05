a26db6cd2a272cd5286d2f944a482356fe91c292
alicek0 <alicekangisy@gmail.com>
2026-04-14 12:39:29 +0900

    chore: remove obsolete dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,7 +40,6 @@ dependencies {
     implementation("io.jsonwebtoken:jjwt-api:0.13.0")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.13.0")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.13.0")
-    implementation("me.paulschwarz:spring-dotenv:4.0.0")
 }
 
 tasks.withType<Test> {
