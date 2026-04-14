d2fe904097f771481a20bc27e2558ef1101d0193
501501a <2501agency@gmail.com>
2026-04-15 01:10:17 +0900

    docs: 피드 API Swagger 문서 추가

--- backend/src/main/java/com/team05/demo/domain/feed/controller/FeedController.java
+++ backend/src/main/java/com/team05/demo/domain/feed/controller/FeedController.java
@@ -9,6 +9,7 @@ import com.team05.demo.domain.feed.dto.FeedRes;
 import com.team05.demo.domain.feed.service.FeedService;
 import com.team05.demo.domain.user.entity.User;
 import com.team05.demo.domain.user.repository.UserRepository;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -26,6 +27,7 @@ public class FeedController {
     private final UserRepository userRepository;
 
     // 댓글 작성
+    @Operation(summary = "피드 댓글 작성")
     @PostMapping("/{feedId}/comments")
     public ResponseEntity<FeedCommentRes> createFeedComment(
             @PathVariable Long feedId,
@@ -34,6 +36,7 @@ public class FeedController {
         return ResponseEntity.ok(res);
     }
 
+    @Operation(summary = "피드 댓글 수정")
     @PutMapping("/{feedId}/comments/{commentId}")
     public ResponseEntity<FeedCommentRes> updateComment(@PathVariable Long commentId,
                                                         @Valid @RequestBody CommentReq commentReq){
@@ -41,13 +44,14 @@ public class FeedController {
         return ResponseEntity.ok(res);
     }
 
+    @Operation(summary = "피드 댓글 삭제")
     @DeleteMapping("/{feedId}/comments/{commentId}")
     public ResponseEntity<Void> deleteComment(@PathVariable Long commentId){
         commentService.deleteFeedComment(commentId);
         return ResponseEntity.noContent().build();
     }
 
-
+    @Operation(summary = "피드 글 작성")
     @PostMapping
     public ResponseEntity<FeedRes> write(
             @RequestBody FeedRequest request) {
@@ -58,6 +62,7 @@ public class FeedController {
         return ResponseEntity.status(201).body(res);
     }
 
+    @Operation(summary = "피드 글 수정")
     @PutMapping("/{feedId}")
     public ResponseEntity<FeedRes> modify(
             @PathVariable Long feedId,
@@ -70,6 +75,7 @@ public class FeedController {
         return ResponseEntity.ok(res);
     }
 
+    @Operation(summary = "피드 글 삭제")
     @DeleteMapping("/{feedId}")
     public ResponseEntity<Void> delete (
             @PathVariable Long feedId
@@ -82,6 +88,7 @@ public class FeedController {
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "피드 상세 조회")
     @GetMapping("/{feedId}")
     public ResponseEntity<FeedRes> getFeed(
             @PathVariable Long feedId
@@ -90,6 +97,7 @@ public class FeedController {
         return ResponseEntity.ok(res);
     }
 
+    @Operation(summary = "피드 목록 조회")
     @GetMapping
     public ResponseEntity<Page<FeedListRes>> getFeeds(
             Pageable pageable
