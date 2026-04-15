57c91569c6fed0802b599f9c64986f361a1aac0d
perandowind <perandowind@gmail.com>
2026-04-15 11:20:32 +0900

    feat: filter 기능(지역, 축종, 상태) 로직 추가

--- backend/src/main/java/com/team05/demo/domain/animal/controller/AnimalController.java
+++ backend/src/main/java/com/team05/demo/domain/animal/controller/AnimalController.java
@@ -12,27 +12,28 @@ public class AnimalController {
 
     private final AnimalService animalService;
 /**
- // todo: 다중정렬 (공고 종료일 같은경우, 다음 정렬 조건), getAnimals(): 필터조건 null체크 & Dto 반환
- @GetMapping()
- @Operation(summary = "유기동물 필터 적용 조회", description = "필터(지역, 종류)와 페이징/정렬을 지원합니다.")
- public ResponseEntity<Page<AnimalResponse>> animalList(
- @RequestParam(required = false) String region,
- @RequestParam(required = false) String kind,
- @PageableDefault(page = 10, sort = "noticeEdt", direction = Sort.Direction.ASC) Pageable pageable
- ) {
+    // todo: 다중정렬 (공고 종료일 같은경우, 다음 정렬 조건), getAnimals(): 필터조건 null체크 & Dto 반환
+    @GetMapping()
+    @Operation(summary = "유기동물 필터 적용 조회", description = "필터(지역, 종류)와 페이징/정렬을 지원합니다.")
+    public ResponseEntity<Page<AnimalResponse>> animalList(
+            @RequestParam(required = false) String region,
+            @RequestParam(required = false) String kind,
+            @RequestParam(required = false) String processState,
+            @PageableDefault(page = 10, sort = "noticeEdt", direction = Sort.Direction.ASC) Pageable pageable
+    ) {
 
- Page<AnimalResponse> response = animalService.getAnimals(region, kind, pageable);
- return ResponseEntity.ok(response);
- }
+        Page<AnimalResponse> response = animalService.getAnimals(region, kind, pageable);
+        return ResponseEntity.ok(response);
+    }
 
- @GetMapping("/{animalId}")
- @Operation(summary = "유기동물 상세 조회")
- public ResponseEntity<AnimalDetailRes> animalDetail(
- @PathVariable Long animalId
- ) {
- AnimalDetailRes response = animalService.getAnimalDetail(animalId);
- return ResponseEntity.ok(response);
- }
+    @GetMapping("/{animalId}")
+    @Operation(summary = "유기동물 상세 조회")
+    public ResponseEntity<AnimalDetailRes> animalDetail(
+            @PathVariable Long animalId
+    ) {
+        AnimalDetailRes response = animalService.getAnimalDetail(animalId);
+        return ResponseEntity.ok(response);
+    }
 
- */
+*/
 }
--- backend/src/main/java/com/team05/demo/domain/animal/service/AnimalService.java
+++ backend/src/main/java/com/team05/demo/domain/animal/service/AnimalService.java
@@ -16,4 +16,52 @@ public class AnimalService {
     public Animal findByAnimalId(Long animalId){
         return animalRepository.findById(animalId).orElseThrow(() -> new BusinessException(AnimalErrorCode.ANIMAL_NOT_FOUND));
     }
+/**
+    public Page<AnimalResponse> getAnimals(String region, String kind, String processState) {
+        List<Animal> allAnimals = animalRepository.findAll();
+
+        // 필터링 로직
+        allAnimals.stream()
+                .filter(animal -> filterByRegion(animal, region))
+                .filter(animal -> filterByKind(animal, kind))
+                .filter(animal -> filterByProcessState(animal, processState))
+                // 이후 entity->DTO 변환,
+
+        // todo: 페이징처리
+
+    }
+*/
+
+
+
+
+    // [지역필터] 강원-원주-2026-00156 에선 "강원"으로 필터링
+    private boolean filterByRegion(Animal animal, String region) {
+        if(region == null || region.isEmpty()) return true;
+        String area = animal.getNoticeNo(); // 강원-원주-2026-00156
+
+        return area.contains(region);
+    }
+
+    // [축종 필터] 개, 고양이, 기타
+    private boolean filterByKind(Animal animal, String kind) {
+        if(kind == null || kind.isEmpty()) return true;
+        String upKindNm = animal.getUpKindNm(); // 개, 고양이, 기타
+
+        if (kind.equals("기타")) {
+            return !upKindNm.equals("개") && !upKindNm.equals("고양이");
+        }
+
+        return upKindNm.contains(kind); // 기타(앵무새) 같은 형식으로 기억해서 contains 사용했습니다.
+    }
+
+
+    // [상태 필터] "보호중", "종료" 포함 여부
+    private boolean filterByProcessState(Animal animal, String processState) {
+        if(processState == null || processState.isEmpty()) return true;
+        return animal.getProcessState().contains(processState);
+    }
+
+
+
 }
