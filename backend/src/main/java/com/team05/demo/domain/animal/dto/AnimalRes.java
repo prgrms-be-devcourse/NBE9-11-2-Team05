50849b2764070630ca09bbb8006d672b30a08f81
perandowind <perandowind@gmail.com>
2026-04-15 15:48:30 +0900

    feat: animalService 정렬,필터,페이징 기능 구현 , AnimalRes Dto 정의, 응답바디에 페이지 정보 추가

--- backend/src/main/java/com/team05/demo/domain/animal/dto/AnimalRes.java
+++ backend/src/main/java/com/team05/demo/domain/animal/dto/AnimalRes.java
@@ -1,4 +1,53 @@
 package com.team05.demo.domain.animal.dto;
 
-public class AnimalRes {
+import com.team05.demo.domain.animal.entity.Animal;
+
+public record AnimalRes(
+        Long animalId,
+        String desertionNo,
+        String processState,
+        String noticeNo,
+        String noticeEdt,
+
+        String upKindNm,
+        String kindFullNm,
+        String colorCd,
+        String age,
+        String weight,
+        String sexCd,
+
+        String popfile1,
+        String popfile2,
+
+        String careNm,
+        String careTel,
+
+        Integer totalCheerCount,
+        double temperature
+) {
+    public AnimalRes(Animal animal) {
+        this(
+                animal.getId(),
+                animal.getDesertionNo(),
+                animal.getProcessState(),
+                animal.getNoticeNo(),
+                String.valueOf(animal.getNoticeEdt()),
+
+                animal.getUpKindNm(),
+                animal.getKindFullNm(),
+                animal.getColorCd(),
+                animal.getAge(),
+                animal.getWeight(),
+                animal.getSexCd(),
+
+                animal.getPopfile1(),
+                animal.getPopfile2(),
+
+                animal.getCareNm(),
+                animal.getCareTel(),
+
+                animal.getTotalCheerCount(),
+                animal.getTotalCheerCount() / (double) 50 // 목표하트수 50 임시설정값
+                );
+    }
 }
