d566ce9a547249a42b48931e41943aae3d1190e5
JW-1126 <jwyoon1126@naver.com>
2026-04-16 14:20:01 +0900

    refactor: 초기 데이터 생성 구조 수정

--- backend/src/main/java/com/team05/petmeeting/global/initData/BaseInitData.java
+++ backend/src/main/java/com/team05/petmeeting/global/initData/BaseInitData.java
@@ -5,18 +5,16 @@ import com.team05.petmeeting.domain.comment.service.CommentService;
 import com.team05.petmeeting.domain.feed.dto.FeedReq;
 import com.team05.petmeeting.domain.feed.dto.FeedRes;
 import com.team05.petmeeting.domain.feed.enums.FeedCategory;
-import com.team05.petmeeting.domain.feed.repository.FeedRepository;
 import com.team05.petmeeting.domain.feed.service.FeedService;
-import com.team05.petmeeting.domain.user.dto.signup.SignupReq;
 import com.team05.petmeeting.domain.user.entity.User;
 import com.team05.petmeeting.domain.user.repository.UserRepository;
-import com.team05.petmeeting.domain.user.service.UserAuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 
 @Configuration
@@ -26,12 +24,11 @@ public class BaseInitData {
     @Autowired
     @Lazy
     private BaseInitData self;
-    private final FeedRepository feedRepository;
     private final FeedService feedService;
     private final CommentService commentService;
 
     private final UserRepository userRepository;
-    private final UserAuthService userAuthService;
+    private final PasswordEncoder passwordEncoder;
 
 
     @Bean
@@ -46,8 +43,9 @@ public class BaseInitData {
         if (userRepository.count() > 0) {
             return;
         }
-        userAuthService.signup(new SignupReq("admin", "12345678Aa!", "admin_nickname", "홍길동"));
-        User user = userRepository.findById(1L).get();
+        User user = userRepository.save(
+                User.create("admin", passwordEncoder.encode("12345678Aa!"), "admin_nickname", "홍길동")
+        );
         FeedRes res1 = feedService.write(new FeedReq(FeedCategory.FREE, "제목1", "내용1", null), user);
         FeedRes res2 = feedService.write(new FeedReq(FeedCategory.FREE, "제목2", "내용2", null), user);
         commentService.createFeedComment(user.getId(), res1.feedId(), new CommentReq("댓글1"));
