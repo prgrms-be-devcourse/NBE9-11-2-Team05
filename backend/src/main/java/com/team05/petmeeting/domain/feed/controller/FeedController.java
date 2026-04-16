54ae02996d41ee0f12e28bb863c417964ed66d70
JW-1126 <jwyoon1126@naver.com>
2026-04-16 19:31:19 +0900

    feat: 게시글 서버 페이지네이션 구현

--- backend/src/main/java/com/team05/petmeeting/domain/feed/controller/FeedController.java
+++ backend/src/main/java/com/team05/petmeeting/domain/feed/controller/FeedController.java
@@ -8,6 +8,7 @@ import com.team05.petmeeting.domain.feed.dto.FeedLikeRes;
 import com.team05.petmeeting.domain.feed.dto.FeedListRes;
 import com.team05.petmeeting.domain.feed.dto.FeedReq;
 import com.team05.petmeeting.domain.feed.dto.FeedRes;
+import com.team05.petmeeting.domain.feed.enums.FeedCategory;
 import com.team05.petmeeting.domain.feed.service.FeedLikeService;
 import com.team05.petmeeting.domain.feed.service.FeedService;
 import com.team05.petmeeting.domain.user.entity.User;
@@ -15,14 +16,22 @@ import com.team05.petmeeting.domain.user.repository.UserRepository;
 import com.team05.petmeeting.global.security.userdetails.CustomUserDetails;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
@@ -123,10 +132,11 @@ public class FeedController {
     @GetMapping
     public ResponseEntity<Page<FeedListRes>> getFeeds(
             Pageable pageable,
-            @AuthenticationPrincipal CustomUserDetails userDetails  // nullable
+            @AuthenticationPrincipal CustomUserDetails userDetails,  // nullable
+            @RequestParam(required = false) FeedCategory category
     ) {
         Long userId = userDetails != null ? userDetails.getUserId() : null;
-        Page<FeedListRes> feeds = feedService.getFeeds(pageable, userId);
+        Page<FeedListRes> feeds = feedService.getFeeds(pageable, userId, category);
         return ResponseEntity.ok(feeds);
     }
 
