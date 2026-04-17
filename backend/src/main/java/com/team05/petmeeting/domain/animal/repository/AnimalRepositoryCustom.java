2767a26cb4b219c41b1ff079a69b664fe2f5483d
perandowind <perandowind@gmail.com>
2026-04-17 20:26:36 +0900

    refac: querydsl DB 페이징 적용.

--- backend/src/main/java/com/team05/petmeeting/domain/animal/repository/AnimalRepositoryCustom.java
+++ backend/src/main/java/com/team05/petmeeting/domain/animal/repository/AnimalRepositoryCustom.java
@@ -2,13 +2,15 @@ package com.team05.petmeeting.domain.animal.repository;
 
 import com.team05.petmeeting.domain.animal.entity.Animal;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface AnimalRepositoryCustom {
 
-    Page<Animal> getAnimals(
+    Page<Animal> findAnimalsWithFilter(
             String region,
             String kind,
-            String processState
+            String processState,
+            Pageable pageable
     );
 
 
